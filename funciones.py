04a5567cd3cc07803aa8ac196648c326769f1e5c
Valenggonzalez <valenngonzalezz70@gmail.com>
2026-06-15 18:18:38 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,1 +1,27 @@
+CAPACIDAD = 50
 
+vehiculos = {}
+
+vehiculos_atendidos = 0
+recaudacion_total = 0
+total_horas = 0
+
+
+def ingresar_vehiculo():
+    print("Función ingresar vehículo")
+
+
+def registrar_egreso():
+    print("Función registrar egreso")
+
+
+def mostrar_estacionados():
+    print("Función mostrar estacionados")
+
+
+def mostrar_disponibles():
+    print("Función mostrar disponibles")
+
+
+def mostrar_estadisticas():
+    print("Función mostrar estadísticas")
